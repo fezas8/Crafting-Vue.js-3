ce59181d4c5b2474c063c05a27e978a9bc007a4a
M-S <mariam.shene@gmail.com>
2023-09-09 22:16:59 +0200

    add hero image, text and button to home

--- crafting-vue-js/src/views/HomeView.vue
+++ crafting-vue-js/src/views/HomeView.vue
@@ -2,13 +2,21 @@
 
 <template>
   <main>
-    <section class="hero is-primary is-fullheight">
+    <section class="hero is-fullheight">
       <div class="hero-body">
-        <div class="">
-          <p class="title">Soar for less</p>
-          <p class="subtitle">50% off for summer</p>
+        <div class="container has-text-centered">
+          <p class="title is-1">Soar for less</p>
+          <p class="subtitle is-3">50% off for summer</p>
+          <button class="button is-link is-medium">Book a flight</button>
         </div>
       </div>
     </section>
   </main>
 </template>
+<style scoped>
+.hero {
+  background-image: url(https://images.unsplash.com/photo-1586500036065-bdaeac7a4feb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3088&q=80);
+  background-position: center;
+  background-size: cover;
+}
+</style>
