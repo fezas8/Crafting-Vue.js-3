9165df54a001dba6350348ac6dda002d91a6c172
M-S <mariam.shene@gmail.com>
2023-09-09 20:43:35 +0200

    add bulma css

--- crafting-vue-js/src/views/HomeView.vue
+++ crafting-vue-js/src/views/HomeView.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <h1>Helloworld</h1>
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="">
+          <p class="title">Soar for less</p>
+          <p class="subtitle">50% off for summer</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
