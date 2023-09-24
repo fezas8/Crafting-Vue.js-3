4added6e64917f252cea5287e4e044ce48cb5941
M-S <mariam.shene@gmail.com>
2023-09-24 22:18:52 +0200

    modify form elements and add values

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,6 @@ const bookingDisabled = ref(false);
 const themeClass = ref('is-link');
 const textAlignment = ref('has-text-centered');
 const containerClasses = ['container', textAlignment.value];
-
 </script>
 
 <template>
@@ -27,42 +26,46 @@ const containerClasses = ['container', textAlignment.value];
       <div class="hero-body">
         <div :class="containerClasses">
           <h1 class="title is-1">{{ title }}</h1>
-          <div class="fields-wrapper">
-            <div>
-              <div class="select" :class="themeClass">
-                <select>
-                  <option>Travel from</option>
-                  <option>Copenhagen</option>
-                  <option>Amsterdam</option>
-                  <option>New York</option>
-                  <option>Bangalore</option>
-                </select>
+          <form class="form-wrapper">
+            <div class="field is-grouped is-grouped-centered">
+              <div class="control">
+                <div class="select" :class="themeClass">
+                  <select value="Copenhagen">
+                    <option>Travel from</option>
+                    <option>Copenhagen</option>
+                    <option>Amsterdam</option>
+                    <option>New York</option>
+                    <option>Bangalore</option>
+                  </select>
+                </div>
+              </div>
+              <div class="control">
+                <div class="select" :class="themeClass">
+                  <select value="New York">
+                    <option>Travel to</option>
+                    <option>Copenhagen</option>
+                    <option>Amsterdam</option>
+                    <option>New York</option>
+                    <option>Bangalore</option>
+                  </select>
+                </div>
+              </div>
+              <div class="control">
+                <input class="input" :class="themeClass" type="date" placeholder="Enter date" value="2025-09-22" />
+              </div>
+              <div class="control">
+                <input class="input" :class="themeClass" type="number" placeholder="1 Adult" value="1" />
+              </div>
+              <div class="control">
+                <input class="input" :class="themeClass" type="number" placeholder="1 Child" value="0"/>
               </div>
             </div>
-            <div>
-              <div class="select" :class="themeClass">
-                <select>
-                  <option>Travel to</option>
-                  <option>Copenhagen</option>
-                  <option>Amsterdam</option>
-                  <option>New York</option>
-                  <option>Bangalore</option>
-                </select>
-              </div>
+            <div class="control">
+              <button class="button is-warning is-medium" :disabled="bookingDisabled" type="submit">
+                {{ bookBtnContent }}
+              </button>
             </div>
-            <div>
-              <input class="input" :class="themeClass" type="date" placeholder="Enter date" />
-            </div>
-            <div>
-              <input class="input" :class="themeClass" type="number" placeholder="1 Adult" />
-            </div>
-            <div>
-              <input class="input" :class="themeClass" type="number" placeholder="1 Child" />
-            </div>
-          </div>
-          <button class="button is-warning is-medium" :disabled="bookingDisabled">
-            {{ bookBtnContent }}
-          </button>
+          </form>
         </div>
       </div>
     </section>
@@ -70,17 +73,12 @@ const containerClasses = ['container', textAlignment.value];
   </main>
 </template>
 <style scoped>
-.fields-wrapper {
-  margin: 0 auto;
-  display: grid;
-  grid-auto-flow: column;
-  gap: 16px;
-  width: fit-content;
-  margin-bottom: 16px;
+.form-wrapper {
+
 }
 .image-section {
   height: 70vh;
-background-image: linear-gradient(to bottom, rgba(255, 255, 0, 0.5), rgba(0, 0, 255, 0.5)),
+  background-image: linear-gradient(to bottom, rgba(255, 255, 0, 0.5), rgba(0, 0, 255, 0.5)),
     url(https://images.unsplash.com/photo-1682687982141-0143020ed57a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3087&q=80);
   background-position: center;
   background-size: cover;
