dbddb7fb8d60e0c80a08446b9ae8ceb2a4842e2c
M-S <mariam.shene@gmail.com>
2023-09-28 19:48:19 +0200

    add more more fields and checks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,32 +11,35 @@ const submitMessage = ref('');
 const isBookingSubmitted = ref(false);
 
 const cities = [
-  "Dubai",
-  "Amsterdam",
-  "Paris",
-  "Copenhagen",
-  "New Delhi",
-  "New York",
-  "Los Angeles",
-  "London",
-  "Oslo"
+  'Dubai',
+  'Amsterdam',
+  'Paris',
+  'Copenhagen',
+  'New Delhi',
+  'New York',
+  'Los Angeles',
+  'London',
+  'Oslo'
 ];
 
 const onSubmit = (): void => {
   isBookingSubmitted.value = true;
-  submitMessage.value = `Booked from ${formData.value.from} to ${formData.value.to}, dated ${formData.value.date} for ${formData.value.adults} adult(s) and ${formData.value.children} child/children`;
+  console.log("formData", formData.value);
+  submitMessage.value = "Booking successful!"
 };
 
 const onBlurOut = () => {
-  isBookingSubmitted.value = false
-}
+  isBookingSubmitted.value = false;
+};
 
 const formData = ref({
-  from: "",
-  to: "",
-  date: "",
+  from: '',
+  to: '',
+  date: '',
   adults: 0,
-  children: 0
+  children: 0,
+  type: 1,
+  returnDate: ''
 });
 
 </script>
@@ -60,6 +63,16 @@ const formData = ref({
           <h1 class="title is-1">{{ title }}</h1>
           <form class="form-wrapper">
             <div class="field is-grouped is-grouped-centered">
+              <div class="control">
+                <label class="radio">
+                  <input type="radio" name="answer" v-model="formData.type" :value="1"/>
+                  One-way
+                </label>
+                <label class="radio">
+                  <input type="radio" name="answer" v-model="formData.type" :value="2"/>
+                  Two-way
+                </label>
+              </div>
               <div class="control">
                 <div class="select" :class="themeClass">
                   <select v-model="formData.from">
@@ -93,6 +106,16 @@ const formData = ref({
                 <input
                   class="input"
                   :class="themeClass"
+                  type="date"
+                  placeholder="Enter return date"
+                  v-model="formData.returnDate"
+                  :disabled="formData.type === 1"
+                />
+              </div>
+              <div class="control">
+                <input
+                  class="input"
+                  :class="themeClass"
                   type="number"
                   placeholder="1 Adult"
                   v-model="formData.adults"
@@ -109,11 +132,16 @@ const formData = ref({
               </div>
             </div>
           </form>
-          <button class="button is-warning is-medium" :disabled="bookingDisabled" @click="onSubmit" @blur="onBlurOut">
+          <button
+            class="button is-warning is-medium"
+            :disabled="bookingDisabled"
+            @click="onSubmit"
+            @blur="onBlurOut"
+          >
             {{ bookBtnContent }}
           </button>
           <div v-if="isBookingSubmitted" class="notification is-success">{{ submitMessage }}</div>
-          <p v-else> To book a flight please fill in the above fields and click 'submit' </p>
+          <p v-else>To book a flight please fill in the above fields and click 'submit'</p>
         </div>
       </div>
     </section>
@@ -124,6 +152,12 @@ const formData = ref({
 .form-wrapper,
 .button {
   margin-bottom: 24px;
+}
+.field {
+  flex-wrap: wrap;
+}
+.control {
+  margin-top: 10px;
 }
 .image-section {
   height: 70vh;
